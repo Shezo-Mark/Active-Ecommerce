0fdc701458ba75fbf0676965981ad60bd1c45ec5
shehzad <shehzadamin697@gmail.com>
2025-08-17 17:51:55 +0500

    save

--- src/app/services/user-auth.service.ts
+++ src/app/services/user-auth.service.ts
@@ -1,9 +1,14 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { SessionService } from '../core/session.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserAuthService {
 
-  constructor() { }
+  constructor(public http:HttpClient, public session:SessionService) { }
+    UserRagisterBck(data: any){
+     return this.http.post<any>(this.session.baseurl+'UserAuth', data);
+  }
 }
--- src/app/web-files/user-auth/user-signup/user-signup.component.ts
+++ src/app/web-files/user-auth/user-signup/user-signup.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormControlName, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { UserAuthService } from '../../../services/user-auth.service';
 
 @Component({
@@ -10,12 +10,56 @@ import { UserAuthService } from '../../../services/user-auth.service';
 })
 export class UserSignupComponent {
   constructor(public user:UserAuthService){}
-   UserRagister = new FormGroup({
-     name: new FormControl('', Validators.required),
-     email: new FormControl('', Validators.required),
-     phone: new FormControl('', Validators.required),
-     password: new FormControl('', Validators.required),
-   })
-   UserRagisterForm(data: any){
+  showpassword: boolean =  false;
+  confirmshowpassword: boolean =  false;
+  SuccessMessage=  '';
+  ErrorMessage=  '';
+  passwordMismatch = false;
+   UserRagister = new FormGroup ({
+    name: new FormControl('', [Validators.required, Validators.minLength(2)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    phone: new FormControl('', [Validators.required, Validators.pattern(/^\d{10}$/)]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    confirmPassword: new FormControl('', [Validators.required])
+   });
+
+     ngOnInit() {
+      this.UserRagister.get('password')?.valueChanges.subscribe(() => this.checkPasswords());
+      this.UserRagister.get('confirmPassword')?.valueChanges.subscribe(() => this.checkPasswords());
+    }
+    checkPasswords() {
+      const pass = this.UserRagister.get('password')?.value;
+      const confirmPass = this.UserRagister.get('confirmPassword')?.value;
+      this.passwordMismatch = pass !== confirmPass;
+    }
+    UserRagisterForm(){
+    if(this.UserRagister.invalid || this.passwordMismatch){
+       console.log('Form Invalid')
+       this.UserRagister.markAllAsTouched();
+       return
+    }else{
+      this.user.UserRagisterBck(this.UserRagister.value).subscribe((res) => {
+        console.log('Backend Response', res);
+        this.SuccessMessage = 'Form Submit Successfully';
+        setTimeout(() => {
+          this.SuccessMessage=  '';
+        }, 2000);
+        this.UserRagister.reset();
+      }, (err) => {
+        console.error('Backend Error', err);
+       this.ErrorMessage = 'Form Submit Faild';
+        setTimeout(() => {
+          this.ErrorMessage=  '';
+        }, 1000);
+      }
+    )
+    }
    }
+ PasswordToggler(){
+  this.showpassword = !this.showpassword;
+ }
+ ConfirmPasswordToggler(){
+  this.confirmshowpassword = !this.confirmshowpassword;
+ }
+
 }
